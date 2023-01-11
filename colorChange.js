8545f1f8915d9ff413f236ebf34dbe3198b4e22d
davedude1011 <105269128+davedude1011@users.noreply.github.com>
2023-01-11 18:16:36 +0000

    Add files via upload

--- colorChange.js
+++ colorChange.js
@@ -5,6 +5,7 @@ function lightDarkMode() {
     if (lightMode == "true") {
         document.documentElement.style.setProperty('--bg-color', '#0d1117');
         document.documentElement.style.setProperty('--bg-accent-color', '#1b222d');
+        document.documentElement.style.setProperty('--bg-accent-color-lower', '#1b222d5c');
         document.documentElement.style.setProperty('--text-color', 'white');
         document.documentElement.style.setProperty('--note-hover-color', 'rgba(255, 255, 255, 0.15)');
         lightMode = "false"
@@ -13,6 +14,7 @@ function lightDarkMode() {
     else {
         document.documentElement.style.setProperty('--bg-color', '#f2eee8');
         document.documentElement.style.setProperty('--bg-accent-color', '#e4ddd2');
+        document.documentElement.style.setProperty('--bg-accent-color-lower', '#E4DDD2A0');
         document.documentElement.style.setProperty('--text-color', 'black');
         document.documentElement.style.setProperty('--note-hover-color', 'rgba(0, 0, 0, 0.15)');
         lightMode = "true"
@@ -24,12 +26,14 @@ function lightDarkMode() {
 if (lightMode == "true") {
     document.documentElement.style.setProperty('--bg-color', '#f2eee8');
     document.documentElement.style.setProperty('--bg-accent-color', '#e4ddd2');
+    document.documentElement.style.setProperty('--bg-accent-color-lower', '#E4DDD2A0');
     document.documentElement.style.setProperty('--text-color', 'black');
     document.documentElement.style.setProperty('--note-hover-color', 'rgba(0, 0, 0, 0.15)');
 }
 else {
     document.documentElement.style.setProperty('--bg-color', '#0d1117');
     document.documentElement.style.setProperty('--bg-accent-color', '#1b222d');
+    document.documentElement.style.setProperty('--bg-accent-color-lower', '#1b222d5c');
     document.documentElement.style.setProperty('--text-color', 'white');
     document.documentElement.style.setProperty('--note-hover-color', 'rgba(255, 255, 255, 0.15)');
 }
